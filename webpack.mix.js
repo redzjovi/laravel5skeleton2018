b0de0e00c80d06f433bfaed888384a4895a771a1
Jovi <redzjovi@gmail.com>
2019-01-20 18:37:59 +0700

    [Cms] add app.js, update vue.

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,6 +14,8 @@ let mix = require('laravel-mix');
 // mix.js('resources/js/app.js', 'public/js')
 //     .sass('resources/sass/app.scss', 'public/css');
 
+mix.js('resources/js/app.js', 'public/js');
+
 mix.copy('node_modules/@fortawesome/fontawesome-free/webfonts/*', 'public/webfonts')
 .styles([
     'node_modules/@fortawesome/fontawesome-free/css/all.min.css',
