e21b0ea8a2919e3dc54798e6120e89d5a43b1b31
Jovi <redzjovi@gmail.com>
2019-02-17 07:22:48 +0700

    [Cms][Npm] update bootstrap, popper.js.

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,24 +11,19 @@ let mix = require('laravel-mix');
  |
  */
 
-// mix.js('resources/js/app.js', 'public/js')
-//     .sass('resources/sass/app.scss', 'public/css');
-
-mix.js('resources/js/app.js', 'public/js');
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css');
 
 mix.copy('node_modules/@fortawesome/fontawesome-free/webfonts/*', 'public/webfonts')
 .styles([
     'node_modules/@fortawesome/fontawesome-free/css/all.min.css',
-    'node_modules/bootstrap/dist/css/bootstrap.min.css', 'Modules/Cms/Resources/assets/node_modules/bootstrap/bootstrap-custom.css',
+    'Modules/Cms/Resources/assets/node_modules/bootstrap/bootstrap-custom.css',
     'node_modules/bootstrap-select/dist/css/bootstrap-select.min.css',
     'node_modules/select2/dist/css/select2.min.css',
 ], 'public/css/backend--app.css');
 
 mix.combine([
-    'node_modules/jquery/dist/jquery.min.js',
-
     'node_modules/@fortawesome/fontawesome-free/js/all.min.js',
-    'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
     'node_modules/bootstrap-select/dist/js/bootstrap-select.min.js',  'Modules/Cms/Resources/assets/node_modules/bootstrap-select/bootstrap-select.js',
     'node_modules/pjax/pjax.min.js', 'Modules/Cms/Resources/assets/node_modules/pjax/pjax-custom.js',
     'node_modules/select2/dist/js/select2.full.min.js', 'Modules/Cms/Resources/assets/node_modules/select2/select2-custom.js',
